015f8a65d2a47a04c7108d5efaadab02aaaefa7e
nhinguyen1011 <75206214+nhinguyen1011@users.noreply.github.com>
2021-11-10 12:20:45 +0700

    Update algorithms.cpp

--- DSA-Nhi/DSA-main/algorithms.cpp
+++ DSA-Nhi/DSA-main/algorithms.cpp
@@ -9,18 +9,20 @@ void swap(int &a, int &b)
 }
 
 // 1.SelectionSort
-void selectionSortWithComparison(int a[], int n, int &count_compare)
+void selectionSortWithComparison(int a[], int n)
 {
-    int j. min;
-    for (int i = 0; ++count_compare && i < n; i++)
+    int  j,  min;
+	unsigned int count_compare = 0;
+    for (int i = 0; ++count_compare && i < n  ; i++)
     {
-        min = i;
-        for (j = i + 1; ++count_compare && j < n; j++)
-            if (++count_compare && a[j] < a[min])
-            {
-                swap(a[min], a[j]);
-            }
+		min = i;
+    	for (j = i + 1; ++count_compare && j < n; j++)
+        	if (++count_compare && a[j] < a[min])
+        	 {
+        	    HoanVi(a[min], a[j]);
+             }
     }
+	cout << "The number of comparisons in the selectionsort algorithm: " << count_compare << endl;
 }
 void selectionSortWithTime(int a[], int n)
 {
@@ -154,50 +156,78 @@ void mergeSortWithComparison(int array[], int const begin, int const end, int &c
 }
 
 // 9.RadixSort
-void radixSortWithComparison(int a[], int n, int &count_compare)
+int MaxNumber(int a[], int n,unsigned int &count_compare )
 {
-    int b[n], temp, i;
-    int box[10] = {0}, m = a[0];
-
-    for (i = 0; ++count_compare && i < n; i++)
-    {
+    int m = a[0];
+	count_compare = 0;
+    for (int i = 1; ++count_compare && i < n; i++)
         if (++count_compare && a[i] > m)
             m = a[i];
+    return m;
+}
+void Count(int arr[], int n, int exp, unsigned int &count_compare)
+{
+    int bucket[n]; 
+    int i, box[10] = { 0 };
+ 	count_compare = 0;
+
+    for (i = 0; ++count_compare && i < n; i++)
+        box[(arr[i] / exp) % 10]++;
+ 
+    for (i = 1; ++count_compare && i < 10; i++)
+        box[i] += box[i - 1];
+ 
+    for (i = n - 1; ++count_compare && i >= 0; i--) {
+        bucket[box[(arr[i] / exp) % 10] - 1] = arr[i];
+        box[(arr[i] / exp) % 10]--;
     }
-    while (++count_compare && m / temp > 0)
-    {
-        for (i = 0; ++count_compare && i < n; i++)
-            box[a[i] / temp % 10]++;
-        for (i = 1; ++count_compare && i < 10; i++)
-            box[i] += box[i - 1];
-        for (i = n - 1; ++count_compare && i >= 0; i--)
-            b[--box[a[i] / temp % 10]] = a[i];
-        for (i = 0; ++count_compare && i < n; i++)
-            a[i] = b[i];
-        temp *= 10;
-    }
+ 
+    for (i = 0; ++count_compare && i < n; i++)
+        arr[i] = bucket[i];
+}
+ 
+ 
+void radixSortWithComparison(int a[], int n)
+{
+    unsigned int count_compare = 0;
+	int m =  MaxNumber(a, n, count_compare);
+ 
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        Count(a, n, exp, count_compare);
+    cout << "The number of comparisons in the algorithm: " << count_compare;
 }
 
-void radixSortWithTime(int a[], int n)
+int MaxNumber1(int a[], int n )
 {
-    int b[n], temp, i;
-    int box[10] = {0}, m = a[0];
-
-    for (i = 0; i < n; i++)
-    {
+    int m = a[0];
+    for (int i = 1;  i < n; i++)
         if (a[i] > m)
             m = a[i];
+    return m;
+}
+void Count1(int arr[], int n, int exp)
+{
+    int bucket[n]; 
+    int i, box[10] = { 0 };
+
+    for (i = 0;  i < n; i++)
+        box[(arr[i] / exp) % 10]++;
+ 
+    for (i = 1; i < 10; i++)
+        box[i] += box[i - 1];
+ 
+    for (i = n - 1; i >= 0; i--) {
+        bucket[box[(arr[i] / exp) % 10] - 1] = arr[i];
+        box[(arr[i] / exp) % 10]--;
     }
-    while (m / temp > 0)
-    {
-        for (i = 0; i < n; i++)
-            box[a[i] / temp % 10]++;
-        for (i = 1; i < 10; i++)
-            box[i] += box[i - 1];
-        for (i = n - 1; i >= 0; i--)
-            b[--box[a[i] / temp % 10]] = a[i];
-        for (i = 0; i < n; i++)
-            a[i] = b[i];
-        temp *= 10;
-    }
+ 
+    for (i = 0; i < n; i++)
+        arr[i] = bucket[i];
+}
+void radixSortWithTime(int a[], int n)
+{
+	int m =  MaxNumber1(a, n);
+ 
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        Count1(a, n, exp);
 }
